a164ee4740bae4c534d2b297b5bdf8fdb282f68e
hoafrankie <hoa.pianist.0353151757@gmail.com>
2024-09-10 22:15:36 +0700

    tesst

---
+++ src/main/java/controller/homeController.java
@@ -0,0 +1,6 @@
+package controller;
+
+public class homeController {
+    private int id;
+    private String ten;
+}
---
+++ src/test/java/com/example/tshirt_luxury_datn/TshirtLuxuryDatnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tshirt_luxury_datn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TshirtLuxuryDatnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
