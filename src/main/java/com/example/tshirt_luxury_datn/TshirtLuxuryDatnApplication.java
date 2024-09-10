127cc6dd9d82bc57a41f617465bc33782d0cd430
High Lam <hhighlam2004@gmail.com>
2024-09-10 22:00:10 +0700

    Dang cap

---
+++ src/main/java/com/example/tshirt_luxury_datn/TshirtLuxuryDatnApplication.java
@@ -0,0 +1,13 @@
+package com.example.tshirt_luxury_datn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TshirtLuxuryDatnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TshirtLuxuryDatnApplication.class, args);
+    }
+
+}
