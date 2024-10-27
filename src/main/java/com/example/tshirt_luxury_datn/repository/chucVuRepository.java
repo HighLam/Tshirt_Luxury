67a9ce8871e7835192b2a3a31b4fe95a7ce7e2ba
hdmmanh <hdmmanh26@gmail.com>
2024-10-27 23:06:38 +0700

    MinhManh push CRUD  NguoiDung

---
+++ src/main/java/com/example/tshirt_luxury_datn/repository/chucVuRepository.java
@@ -0,0 +1,7 @@
+package com.example.tshirt_luxury_datn.repository;
+
+import com.example.tshirt_luxury_datn.entity.ChucVu;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface chucVuRepository extends JpaRepository<ChucVu, Integer> {
+}
